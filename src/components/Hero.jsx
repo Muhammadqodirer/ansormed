83ff32687a9e10b9d483d731613c63f85b50ae0c
MuhammadqodirErgashboyev <pythonfn26@gmail.com>
2025-10-06 21:00:24 +0500

    qoshildi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,13 @@ function Hero() {
         <div className="hero-parent">
           <div className="hero-content">
             <h2 className="hero-title">
-              Tabiiy davolash usullari yordamida kasalliklardan xalos bo’ling.
+              Tabiiy davolash usullari yordamida <br /> 
+               kasalliklardan xalos bo’ling.
             </h2>
             <p className="hero-text">
-              Hijoma, manual terapiya, zuluk va turli tabiiy giyohlar eng ko’p
-              uchraydigan kasalliklarni yengishda yordam beradi. Sog’ligingizni
-              o’z ishining professionallariga ishonib topshiring.
+              Hijoma, manual terapiya, zuluk va turli tabiiy <br />
+               giyohlar eng ko’p uchraydigan kasalliklarni <br /> yengishda yordam beradi. Sog’ligingizni
+              o’z ishining <br /> professionallariga ishonib topshiring.
             </p>
             <a href="#" className="hero-btn">
               Qabulga yozilish
